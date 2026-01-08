1aa2dd11ce894cb53fd9b0f5f278adb37c674a35
ddeepak95 <ddeepak95@gmail.com>
2026-01-08 14:31:41 +0530

    malayalam support added

--- LANGUAGE_CONSTANTS.py
+++ LANGUAGE_CONSTANTS.py
@@ -6,4 +6,5 @@ LANGUAGES = {
     "de": {"pipecat_language": Language.DE, "name": "German", "cartesia_voice_id": "1ade29fc-6b82-4607-9e70-361720139b12"},
     "hi": {"pipecat_language": Language.HI, "name": "Hindi", "cartesia_voice_id": "95d51f79-c397-46f9-b49a-23763d3eaa2d"},
     "kn": {"pipecat_language": Language.KN, "name": "Kannada", "cartesia_voice_id": "6baae46d-1226-45b5-a976-c7f9b797aae2"},
+    "ml": {"pipecat_language": Language.ML, "name": "Malayalam", "cartesia_voice_id": "374b80da-e622-4dfc-90f6-1eeb13d331c9"},
 }
