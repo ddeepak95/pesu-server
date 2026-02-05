d1dad620325ee6890d482ef0df048b60cf9f07c2
ddeepak95 <ddeepak95@gmail.com>
2026-02-05 09:25:45 -0500

    english indian accent added

--- LANGUAGE_CONSTANTS.py
+++ LANGUAGE_CONSTANTS.py
@@ -3,6 +3,7 @@ from pipecat.transcriptions.language import Language
 LANGUAGES = {
     "ta": {"pipecat_language": Language.TA, "name": "Tamil", "cartesia_voice_id": "7f98e662-142d-41ba-89a2-12452640ce6d"},
     "en": {"pipecat_language": Language.EN, "name": "English", "cartesia_voice_id": "8d8ce8c9-44a4-46c4-b10f-9a927b99a853"},
+    "en-IN": {"pipecat_language": Language.EN_IN, "name": "English (India)", "cartesia_voice_id": "638efaaa-4d0c-442e-b701-3fae16aad012"},
     "de": {"pipecat_language": Language.DE, "name": "German", "cartesia_voice_id": "1ade29fc-6b82-4607-9e70-361720139b12"},
     "hi": {"pipecat_language": Language.HI, "name": "Hindi", "cartesia_voice_id": "95d51f79-c397-46f9-b49a-23763d3eaa2d"},
     "kn": {"pipecat_language": Language.KN, "name": "Kannada", "cartesia_voice_id": "6baae46d-1226-45b5-a976-c7f9b797aae2"},
